a5dd1c00b6c6ef5c37037632f271938914cc5068
Chance Nielsen <chance_nielsen.stu@lusherschool.org>
2022-06-02 21:00:20 +0000

    muffindohoah.github.io/wiki.html for opspark

--- asd-projects/debugging-exercise/index.js
+++ asd-projects/debugging-exercise/index.js
@@ -140,7 +140,7 @@ function bounceCircle(circle){
 
 // this redraws the circle's position on the screen
 function updateCircleOnScreen(circle){
-    maxCircles = 0;
+    
 
     // these lines redraw the circle's position
     $(circle.id).css('left', circle.x);
